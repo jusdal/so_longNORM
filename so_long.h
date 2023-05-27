0baf1ca699131a9abfe55d349f0ea700d4439d03
justin daly <justin.m.daly@gmail.com>
2023-05-28 01:44:08 +0700

    update header file

---
+++ so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 01:08:23 by jdaly             #+#    #+#             */
+/*   Updated: 2023/05/28 01:43:29 by jdaly            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "gnl/get_next_line.h"
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <fcntl.h>
+
+#endif
