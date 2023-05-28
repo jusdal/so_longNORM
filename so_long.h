c200ba976dc9e067dbf53523c789a3c422fa8c3e
justin daly <justin.m.daly@gmail.com>
2023-05-29 00:11:43 +0700

    update header file

--- so_long.h
+++ so_long.h
@@ -6,18 +6,46 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 01:08:23 by jdaly             #+#    #+#             */
-/*   Updated: 2023/05/28 01:43:29 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/05/29 00:11:21 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "gnl/get_next_line.h"
-# include "libft/libft.h"
+# include "get_next_line.h"
+//# include "libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdbool.h>
 # include <fcntl.h>
+
+typedef struct s_vector
+{
+	int	x;
+	int	y;
+} t_vec;
+typedef	struct s_mapdata
+{
+	int		width;
+	int		height;
+	int		point_row;
+	int		point_column;
+	t_vec	player;
+	t_vec	exit;
+	int		n_player;
+	int		n_exit;
+	int		n_collect;
+}	t_mapdata;
+
+/* mapchecker.c */
+void	error(char *message);
+void	free_array(char **array);
+void	free_error(char *message, char** array);
+
+/* flood fill.c */
+void	fill(char **array, t_mapdata *data, int x, int y);
+void	ff_map(char **maparray, t_mapdata *data);
+void	check_path(char **maparray, t_mapdata *data);
 
 #endif
