ba298c3f0d00234f4e235ee3776e95fa36e5fa5d
justin daly <justin.m.daly@gmail.com>
2023-05-30 17:06:03 +0700

    update solong header

--- so_long.h
+++ so_long.h
@@ -6,20 +6,21 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 01:08:23 by jdaly             #+#    #+#             */
-/*   Updated: 2023/05/29 12:12:07 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/05/30 15:53:04 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "get_next_line.h"
+# include "gnl/get_next_line.h"
 //# include "libft/libft.h"
 # include <fcntl.h>
 # include "mlx/mlx.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdbool.h>
+# include <string.h> //remove and replace strcmp in 1checkmap.c
 
 
 typedef struct s_vector
