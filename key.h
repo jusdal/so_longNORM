d60d95417b918ee66a5b76dd3f3e528038b2a1f8
justin daly <justin.m.daly@gmail.com>
2023-05-31 16:06:18 +0700

    add key.h

---
+++ key.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 14:43:27 by jdaly             #+#    #+#             */
+/*   Updated: 2023/05/31 14:59:30 by jdaly            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEY_H
+# define KEY_H
+
+# define KEY_ESC    53
+
+# define KEY_A      0
+# define KEY_D      2
+# define KEY_W      13
+# define KEY_S      1
+# define KEY_UP     126
+#define KEY_DOWN    125
+#define KEY_LEFT    123
+#define KEY_RIGHT   124
+
+#endif
